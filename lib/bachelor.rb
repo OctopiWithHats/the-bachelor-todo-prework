d1b7f134ac54a3ef001cfc337dd38de4b07a4231
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 23:40:35 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -40,7 +40,15 @@ end
 
 def get_occupation(data, hometown)
   # code here
-  
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |a, b|
+        if b == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
